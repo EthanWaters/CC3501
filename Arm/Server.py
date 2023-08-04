1205f9a53d6145af1499367cfaaed7dd266b4608
EthanWaters <ethan.waters@my.jcu.edu.au>
2023-08-04 17:05:36 +1000

    working on list

--- Arm/Server.py
+++ Arm/Server.py
@@ -1,56 +1,78 @@
 import socket
 from socket import SOCK_DGRAM, SO_REUSEADDR
 import threading
+from typing import Any
 from Client import Client
+import queue
 
 class Server:
     def __init__(self, HOST="127.0.0.1", PORT=54321, BUFF_SIZE=1024):
         self.s = socket.socket(type=SOCK_DGRAM)
         self.HOST = HOST
         self.PORT = PORT
-        self.recieving_data = False
-        self.recieved_data = []
-        
+        self.receiving_data = False
+        # self.received_data = [] 
+        self.received_data = None
+        self.test = queue.Queue()
+
         #self.MAX_CONNECTIONS = MAX_CONNECTIONS
-        self.BUFF_SIZE = BUFF_SIZE
+        self.BUFF_SIZE = BUFEF_SIZ
         self.s.bind((self.HOST, self.PORT))
         # self.s.listen(self.MAX_CONNECTIONS)
     
         print("Starting a server")
         print(str(self.HOST)+ ":" + str(self.PORT))
         
-        self.thread = threading.Thread(target=self.get_data)
-        self.thread.start()
+        # self.thread = threading.Thread(target=self.get_data)
+        # self.thread.start()
         # self.s.setblocking(0)
-        self.start_recieving()
+        self.start_receiving()
     
     def __repr__(self):
-        return "Server-{}:{} /n{}".format(self.HOST, self.PORT, self.recieved_data)  
+        return "Server-{}:{} /n{}".format(self.HOST, self.PORT, self.received_data)  
         
 
-    def start_recieving(self):
-        self.recieving_data = True
+    def start_receiving(self):
+        self.receiving_data = True
         self.thread = threading.Thread(target=self.get_data)
         self.thread.start()
         print("Started reading data")
     
-    def stop_recieving(self):
-        self.recieving_data = False
+    def stop_receiving(self):
+        self.receiving_data = False
         self.s.close()
         print("Stopped reading data")
 
 
     def get_data(self):
-        self.recieving_data = True
-        while self.recieving_data:
-            data_recieved = self.s.recv(self.BUFF_SIZE)
-            data = data_recieved.decode()           
+        self.received_data = []
+        self.receiving_data = True
+        while self.receiving_data:
+            data = self.s.recv(self.BUFF_SIZE)
+            data = data.decode()
             if data == "end":
-                self.stop_recieving()
-            self.recieved_data.append(data)
-            
-    def get_recieved_data(self):
-        print(self.recieved_data)
-        return self.recieved_data
+                self.stop_receiving()
+            self.test.put(data)
+            self.received_data = self.received_data.append(data) 
+        
+
+    def get_received_data(self):
+        return self.received_data
     
-server = Server()
+
+    def set_received_data(self, value):
+        self.received_data = value
+
+
+    # def get_received_data(self):
+    #     # Retrieve data from the queue
+    #     data = []
+    #     while not self.received_data.empty():
+    #         print("UESSSS")
+    #         data.append(self.received_data.get())
+    #     print("====================")
+    #     print(data)
+    #     return data
+
+       
+# server = Server()
