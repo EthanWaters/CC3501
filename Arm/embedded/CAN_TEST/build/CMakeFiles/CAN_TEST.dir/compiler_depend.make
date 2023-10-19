6f00a6dcb3f29ee7cffff8540cc0973fe02ba50e
EthanWaters <ethan.waters@my.jcu.edu.au>
2023-10-19 18:26:19 +1000

    CAN bus update

---
+++ Arm/embedded/CAN_TEST/build/CMakeFiles/CAN_TEST.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+﻿# Empty compiler generated dependencies file for CAN_TEST.
+# This may be replaced when dependencies are built.
---
+++ Arm/embedded/CAN_TEST/src/CAN_TEST.cpp
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "mcp2515.h"
+#include <time.h>
+#include <iostream>
+
+// SPI 
+#define CS_PIN 29
+#define TX_PIN 27
+#define RX_PIN 28
+#define SPI_CLOCK 16000000 //16MHz
+#define SCK_PIN 26
+
+//CAN bus
+#define SLAVE_ID 0x010
+#define YAW_ID 0x01
+#define PITCH_ID 0x02
+#define ROLL_ID 0x03
+#define PAYLOAD_SIZE 8
+
+// IMU 
+#define SAMPLE_PERIOD (0.055f) // 0.05f replace this with actual sample period
+
+
+can_frame rx;
+can_frame tx;
+
+uint64_t check;
+
+tx.can_dlc = PAYLOAD_SIZE;
+
+
+int main() {
+    stdio_init_all();
+   
+    MCP2515 mcp2515(spi1, CS_PIN, TX_PIN, RX_PIN, SCK_PIN, SPI_CLOCK);
+    
+    mcp2515.reset();
+    mcp2515.setBitrate(CAN_1000KBPS, MCP_16MHZ);
+    mcp2515.setNormalMode();    
+
+    //Listen loop
+    while(true) {
+
+       
+        if(mcp2515.readMessage(&rx) == MCP2515::ERROR_OK) {
+            printf("New frame from ID: %10x\n", rx.can_id);
+            
+            memcpy(&check, rx.data, sizeof(unint64_t));
+            std::cout << check << std::endl;
+
+            printf("TEST _______________");
+            for (int i = 0; i < PAYLOAD_SIZE; i++) {
+                printf("%02X ", rx.data[i]);
+            }
+            printf("\n");
+
+        }
+    }
+
+    return 0;
+}
