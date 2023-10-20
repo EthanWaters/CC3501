110dd7b68909b4232f3b457f401133df0998be16
EthanWaters <ethan.waters@my.jcu.edu.au>
2023-10-20 16:36:25 +1000

    test :

---
+++ Arm/embedded/Ethan_test/src/Sensor_Board.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "hardware/spi.h"
+
+// Define LSM9DS1 SPI pins and port number
+#define spiPort spi0
+#define PIN_SCK 2
+#define PIN_MOSI 3
+#define PIN_MISO 4
+#define PIN_CS 5
+
+
+
---
+++ Arm/embedded/Ethan_test/src/main.cpp
@@ -0,0 +1,110 @@
+#include "pico/stdlib.h"
+#include <iostream>
+#include <stdbool.h>
+#include <stdio.h>
+// #include <time.h>
+// #include <chrono>
+// #include "FusionArhs.h"
+// #include "FusionAxes.h"
+// #include "FusionCalibration.h"
+// #include "FusionCompass.h"
+// #include "FusionConvention.h"
+// #include "FusionOffset.h"
+// #include "FusionMath.h"
+// #include "pico/binary_info.h"
+#include "hardware/spi.h"
+#include "hardware/i2c.h"
+// #include "drivers/MPU9250_driver.h"
+
+#define SAMPLE_PERIOD (0.055f) // 0.05f replace this with actual sample period
+#define I2C_SDA_PIN 7
+#define I2C_SCL_PIN 6
+#define SA01 4
+#define CS 5
+#define AG_A 0xD4
+#define M_A 0x38
+
+int16_t accel[3];
+int16_t gyro[3];
+int16_t mag[3];
+
+void readRaw(int16_t accel[3], int16_t gyro[3], int16_t mag[3]) {
+    // For this particular device, we send the device the register we want to read
+    // first, then subsequently read from the device. The register is auto incrementing
+    // so we don't need to keep sending the register we want, just the first.
+
+    uint8_t buffer[6];
+
+    // Start reading acceleration registers from register 0x3B for 6 bytes
+    uint8_t val = 0x28;
+    i2c_write_blocking(i2c_default, AG_A, &val, 1, true); // true to keep master control of bus
+    i2c_read_blocking(i2c_default, AG_A, buffer, 6, false);
+
+    for (int i = 0; i < 3; i++) {
+        accel[i] = (buffer[i * 2] << 8 | buffer[(i * 2) + 1]);
+    }
+
+
+    val = 0x18;
+    i2c_write_blocking(i2c_default, AG_A, &val, 1, true);
+    i2c_read_blocking(i2c_default, AG_A, buffer, 6, false);  // False - finished with bus
+
+    for (int i = 0; i < 3; i++) {
+        gyro[i] = (buffer[i * 2] << 8 | buffer[(i * 2) + 1]);;
+    }
+
+
+
+    val = 0x28;
+    i2c_write_blocking(i2c_default, M_A, &val, 1, true);
+    i2c_read_blocking(i2c_default, M_A, buffer, 6, false);  // False - finished with bus
+
+    for (int i = 0; i < 3; i++) {
+        mag[i] = (buffer[i * 2] << 8 | buffer[(i * 2) + 1]);;
+    }
+
+}
+
+
+
+
+int main() {
+
+    stdio_init_all();
+    gpio_init(SA01);
+    gpio_put(SA01, 1);
+    i2c_init(i2c_default, 400 * 1000);
+    gpio_set_function(I2C_SDA_PIN, GPIO_FUNC_I2C);
+    gpio_set_function(I2C_SCL_PIN, GPIO_FUNC_I2C);
+    gpio_pull_up(CS);
+    gpio_pull_up(I2C_SDA_PIN);
+    gpio_pull_up(I2C_SCL_PIN);
+
+
+    //printf("Calibrating...place on level surface...\n");
+    sleep_ms(500);
+    // Calculate Gyro and Accel Offsets  //
+    
+
+    while (true) { // this loop should repeat each time new gyroscope data is available
+        readRaw(accel, gyro, mag);
+        std::cout << "MAG: ";
+        for (int i = 0; i < 3; i++) {
+            std::cout << mag[i] << " ";
+        }
+        std::cout << std::endl;
+
+        std::cout << "ACCEL: ";
+        for (int i = 0; i < 3; i++) {
+            std::cout << accel[i] << " ";
+        }
+        std::cout << std::endl;
+
+         std::cout << "GYRO: ";
+        for (int i = 0; i < 3; i++) {
+            std::cout << gyro[i] << " ";
+        }
+        std::cout << std::endl;
+        
+    }
+}
