23ccb621adc99728f530ea6216342da58e25960f
EthanWaters <ethan.waters@my.jcu.edu.au>
2023-10-28 18:29:46 +1000

    on da grind, got multiple can buss over socket

--- Arm/embedded/rp2040_node/src/rp2040_node.cpp
+++ Arm/embedded/rp2040_node/src/rp2040_node.cpp
@@ -6,15 +6,16 @@
 #include "pico/binary_info.h"
 #include "LSM9DS1_driver.h"
 #include "math.h"
+#include <string.h>
 
-// SPI 
+// IMU
 #define PIN_SA0 4
 #define PIN_CS   5
 #define PIN_SCK  23
 #define PIN_SDA 6
 
 //CAN bus
-#define SLAVE_ID 0x010
+#define SLAVE_ID 0x020
 #define AG_CAL_REQUEST 0x500
 #define MAG_CAL_REQUEST 0x600
 #define START_REQUEST 0x700
@@ -23,22 +24,36 @@
 #define ROLL_ID 0x03
 #define PAYLOAD_SIZE 4
 #define CAN_RESET 15
+#define CS_PIN 29
+#define TX_PIN 27
+#define RX_PIN 28
+#define SPI_CLOCK 1000000 //16MHz
+#define SCK_PIN 26
+
 
 // IMU 
 float PI = 3.14159;
 float rad_conv = (PI/180);
 float pitch_rad, yaw_rad, roll_rad;
-OrientationData imu_data;
+
 
 // CAN bus
-struct can_frame rx;
-struct can_frame tx;
-tx.can_dlc = PAYLOAD_SIZE;
+
+
 
 
 int main() {
-    stdio_init_all();
 
+    stdio_init_all();
+    // Set reset pin of CAN controller high to enable IC
+    gpio_init(CAN_RESET);
+    gpio_set_dir(CAN_RESET, 1);
+    gpio_put(CAN_RESET, 1);
+
+    can_frame rx;
+    can_frame tx;
+    tx.can_dlc = PAYLOAD_SIZE;
+    OrientationData imu_data;
     //Initialise IMU 
     LSM9DS1Driver lsm9ds1(PIN_SA0, PIN_CS, PIN_SCK, PIN_SDA);
     lsm9ds1.initialise_I2C(); // Set up I2C
@@ -49,15 +64,12 @@ int main() {
     lsm9ds1.CalibrateMag(); // Run sensor through calibration process //
     sleep_ms(2000); // you've done well, have a rest //
 
-    // Set reset pin of CAN controller high to enable IC
-    gpio_init(CAN_RESET);
-    gpio_set_dir(CAN_RESET, 1);
-    gpio_put(CAN_RESET, 1);
-
+  
+  
     MCP2515 mcp2515(spi1, CS_PIN, TX_PIN, RX_PIN, SCK_PIN, SPI_CLOCK);
     
     mcp2515.reset();
-    mcp2515.setBitrate(CAN_1000KBPS, MCP_8MHZ);
+    mcp2515.setBitrate(CAN_5KBPS, MCP_8MHZ);
     mcp2515.setNormalMode();    
 
     // // Gyro and accel calibration request
@@ -107,15 +119,15 @@ int main() {
 
                 tx.can_id = SLAVE_ID + YAW_ID;  
                 memcpy(tx.data, &yaw_rad, sizeof(float));
-                mcp2515.sendMessage(tx);  
+                mcp2515.sendMessage(&tx);  
                 
                 tx.can_id = SLAVE_ID + PITCH_ID;  
                 memcpy(tx.data, &pitch_rad, sizeof(float));
-                mcp2515.sendMessage(tx);  
+                mcp2515.sendMessage(&tx);  
 
                 tx.can_id = SLAVE_ID + ROLL_ID;  
                 memcpy(tx.data, &roll_rad, sizeof(float));
-                mcp2515.sendMessage(tx);  
+                mcp2515.sendMessage(&tx);  
             }
         }
     }
